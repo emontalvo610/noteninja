6a64c1492d129a00b7b5274e896109e71e1fba89
emontalvo610 <einarmontalvo610@gmail.com>
2024-01-27 14:49:16 +0530

    feat: base ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,75 @@
+"use client"
+
+import { useState } from "react"
 import Link from "next/link"
+import { useQuery } from "@tanstack/react-query"
+import MDEditor from "@uiw/react-md-editor"
+import axios from "axios"
+import rehypeSanitize from "rehype-sanitize"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
+  const [selectedNote, setSelectedNote] = useState<any>(null)
+
+  const { data: notes } = useQuery({
+    queryKey: ["notes"],
+    queryFn: async () => {
+      const res = await axios.get("/api/notes")
+
+      return res.data.notes
+    },
+  })
+
+  console.log(notes)
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+    <div className="flex max-w-5xl mx-auto mt-16 gap-8">
+      <div className="bg-card rounded-xl py-8 px-8 w-full">
+        {selectedNote ? (
+          <>
+            <Button
+              className="mb-4"
+              variant="ghost"
+              onClick={() => setSelectedNote(null)}
+            >
+              ← Back
+            </Button>
+
+            <h3 className="text-2xl font-semibold">{selectedNote.title}</h3>
+
+            {/* <p className="mt-4">{selectedNote.text}</p> */}
+            <MDEditor
+              value={selectedNote.text}
+              className="mt-8"
+              height={500}
+              previewOptions={{
+                rehypePlugins: [[rehypeSanitize]],
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <h2 className="text-3xl font-bold">Notes</h2>
+            {notes ? (
+              <ul className="mt-8 gap-2 flex flex-col">
+                {notes.map((note: any) => (
+                  <li
+                    key={note.id}
+                    className="cursor-pointer"
+                    onClick={() => setSelectedNote(note)}
+                  >
+                    <h3 className="text-2xl font-semibold">{note.title}</h3>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </>
+        )}
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    </div>
   )
 }
