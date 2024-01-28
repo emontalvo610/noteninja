436cbdc83e41ffffa05a2e5ee43a248a0c49ebd8
emontalvo610 <einarmontalvo610@gmail.com>
2024-01-28 10:42:41 +0530

    chore: change name

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function IndexPage() {
         <section className="flex flex-col">
           <div className="flex flex-col items-start gap-2">
             <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-              Synthax project
+              Noteninja
             </h1>
 
             <div className="flex flex-col max-w-5xl mx-auto mt-16 w-full">
--- components/chat/empty-screen.tsx
+++ components/chat/empty-screen.tsx
@@ -22,7 +22,9 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, "setInput">) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <Card>
-        <h1 className="mb-2 text-lg font-semibold">Welcome to Synthax</h1>
+        <h1 className="mb-2 text-lg font-semibold">
+          Welcome to Noteninja chat
+        </h1>
 
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
